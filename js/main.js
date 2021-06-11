6d46b9de53cec4ca41b858c6158260bb05644988
Ярослав <spartacus215@yandex.ru>
2021-06-11 21:23:21 +0300

    Д/з Больше деталей(сделал)

--- js/main.js
+++ js/main.js
@@ -10,31 +10,15 @@ const getRandomNumber = function (minNum, maxNum, lengthNum = 0) {
     return 'Ошибка. Начальное число не должно быть больше или равно конечному.';
   }
   else {
-    const randomNumber = Math.floor(minNum + Math.random() * (maxNum + 1 - minNum));
+    const randomNumber = minNum + Math.random() * (maxNum - minNum);
     return +randomNumber.toFixed(lengthNum);
   }
 };
-getRandomNumber(0, 100, 1);
-// console.log(getRandomNumber(0, 100));
-
+getRandomNumber();
 
 // ПОЛУЧЕНИЕ МАССИВА ИЗ ДЕСЯТИ СГЕНЕРИРОВАННЫХ ОБЪЕКТОВ
-// Количество объектов(объявлений)
-const QUANTITY_OBJECTS = 10;
-
-// Массив с описанием авторов предложений
-const AUTHORS = [
-  'img/avatars/user01.png',
-  'img/avatars/user02.png',
-  'img/avatars/user03.png',
-  'img/avatars/user04.png',
-  'img/avatars/user05.png',
-  'img/avatars/user06.png',
-  'img/avatars/user07.png',
-  'img/avatars/user08.png',
-  'img/avatars/user09.png',
-  'img/avatars/user10.png',
-];
+// Количество необходимых объектов для генерации
+const QUANTITY_ADS = 10;
 
 // Массив с заголовками предложений
 const TITLES = [
@@ -51,13 +35,13 @@ const TITLES = [
 ];
 
 // Диапазон цен
-const PRICE = {
+const PRICES = {
   MIN: 1000,
   MAX: 100000,
 };
 
 // Типы жилья
-const TYPE_HOUSE = [
+const TYPE_HOUSES = [
   'palace',
   'flat',
   'house',
@@ -71,14 +55,19 @@ const ROOMS = {
   MAX: 5,
 };
 
-// Время начала регистрации в отеле
+const GUESTS = {
+  MIN: 5,
+  MAX: 25,
+};
+
+// Время начала регистрации
 const CHECKIN = [
   '12:00',
   '13:00',
   '14:00',
 ];
 
-// Время окончания суток и выезда из отеля
+// Время окончания суток и выезда
 const CHECKOUT = [
   '12:00',
   '13:00',
@@ -96,7 +85,7 @@ const FEATURES = [
 ];
 
 // Список описаний помещений
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'Можно дышать свежим воздухом не вдыхая смог проезжающего автотранспорта благодаря тому, что окна квартиры выходят на парк.',
   'Отсутствует проблема с парковкой. Всегда свободные парковочные места, можно удобно припарковаться рядом с домом.',
   'Улучшенная планировка и большая площадь. 44 кв.м. общей площади и 9 метровая кухня это гораздо больше, чем в стандартной 1-комнатной квартире.',
@@ -115,3 +104,79 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
+
+// Координаты местонахождения на карте
+const LOCATION = {
+  LAT: {
+    MIN: 35.65000,
+    MAX: 35.70000,
+  },
+  LNG: {
+    MIN: 139.70000,
+    MAX: 139.80000,
+  },
+};
+
+// Массив аватаров
+const arrayAvatars = [];
+
+// Счётчик заполнения массива аватаров URL-ссылками
+for (let imageCounter = 1; imageCounter <= QUANTITY_ADS; imageCounter++) {
+  if (imageCounter < 10) {
+    arrayAvatars.push(`img/avatars/user0${imageCounter}.png`);
+  } else {
+    arrayAvatars.push(`img/avatars/user${imageCounter}.png`);
+  }
+}
+
+// Вырезание/удаление случайного элемента из массива аватаров
+const cutRandomElementArray = function (someArray) {
+  return someArray.splice(getRandomNumber(0, someArray.length - 1), 1);
+};
+
+// Получение рандомного элемента из массива
+const getRandomElementArray = function (someArray) {
+  return someArray[getRandomNumber(0, someArray.length -1)];
+};
+
+// Получение массива случайной длины из другого массива
+const getArrayRandomLength = function (someArray) {
+  const someIndexArray = getRandomNumber(1, someArray.length); // Получение рандомного индекса элемента массива
+  const newRandomArray = [];
+  for (let i = 0; i < someIndexArray; i++) {
+    newRandomArray.push(someArray[i]);
+  }
+  return newRandomArray;
+};
+
+// Функция генерации объектов
+const createAds = function () {
+  const locationX = getRandomNumber(LOCATION.LAT.MIN, LOCATION.LAT.MAX, 5);
+  const locationY = getRandomNumber(LOCATION.LNG.MIN, LOCATION.LNG.MAX, 5);
+  return {
+    author: {
+      avatar: `${cutRandomElementArray(arrayAvatars).join()}`,
+    },
+    offer: {
+      title: `${cutRandomElementArray(TITLES)}`,
+      address: `${locationX}, ${locationY}`,
+      price: `${getRandomNumber(PRICES.MIN, PRICES.MAX)} рублей`,
+      type: `${getRandomElementArray(TYPE_HOUSES)}`,
+      rooms: `${getRandomNumber(ROOMS.MIN, ROOMS.MAX)} комнат(ы)`,
+      guests: `${getRandomNumber(GUESTS.MIN, GUESTS.MAX)} человек(а)`,
+      checkin: `Время заселения ${getRandomElementArray(CHECKIN)}`,
+      checkout: `Время выезда ${getRandomElementArray(CHECKOUT)}`,
+      features: `${getArrayRandomLength(FEATURES).join(', ')}`,
+      description: `${cutRandomElementArray(DESCRIPTIONS)}`,
+      photos: `${getArrayRandomLength(PHOTOS).join(', ')}`,
+    },
+    location: {
+      lat: `${locationX}`,
+      lng: `${locationY}`,
+    },
+  };
+};
+
+const listAds = new Array(QUANTITY_ADS).fill(null).map(() => createAds());
+listAds; // Чтобы eslint не ругался, что listAds не используется
+// console.log(listAds);
