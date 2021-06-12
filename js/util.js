c7ebc8f16cef4af5275b48968ade7930d58b013a
Ярослав <spartacus215@yandex.ru>
2021-06-12 18:52:52 +0300

    Д/з Модуляция(сделал)

---
+++ js/util.js
@@ -0,0 +1,19 @@
+// Функция получения рандомного числа
+const getRandomNumber = function (minNum, maxNum, lengthNum = 0) {
+  if (minNum < 0) {
+    return 'Ошибка. Вы ввели отрицательное число. Число должно быть положительное.';
+  }
+  else if (maxNum <= minNum) {
+    return 'Ошибка. Конечное число меньше или равно начальному.';
+  }
+  else if (minNum >= maxNum) {
+    return 'Ошибка. Начальное число не должно быть больше или равно конечному.';
+  }
+  else {
+    const randomNumber = minNum + Math.random() * (maxNum - minNum);
+    return +randomNumber.toFixed(lengthNum);
+  }
+};
+getRandomNumber();
+
+export {getRandomNumber};
