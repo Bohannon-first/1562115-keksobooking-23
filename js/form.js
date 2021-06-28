d7e9ff8296c298c1af812db329ea9ae015a3046d
Ярослав <spartacus215@yandex.ru>
2021-06-28 21:40:18 +0300

    Правда или действие часть 1(в процессе)

--- js/form.js
+++ js/form.js
@@ -35,3 +35,50 @@ const enableMapFilters = () => {
   }
 };
 enableMapFilters();
+
+
+const inputTitleAd = adForm.querySelector('.ad-form__input-title');
+const MIN_LENGTH_TITLE = 30;
+const MAX_LENGTH_TITLE = 100;
+
+inputTitleAd.addEventListener('input', () => {
+  const lengthValue = inputTitleAd.value.length;
+
+  if (lengthValue < MIN_LENGTH_TITLE) {
+    inputTitleAd.setCustomValidity(`Ещё ${MIN_LENGTH_TITLE - lengthValue} символов`);
+  } else if (lengthValue > MAX_LENGTH_TITLE) {
+    inputTitleAd.setCustomValidity(`Удалите лишние ${lengthValue - MAX_LENGTH_TITLE} символов`);
+  } else {
+    inputTitleAd.setCustomValidity('');
+  }
+
+  inputTitleAd.reportValidity();
+});
+
+const inputPriceAd = adForm.querySelector('.ad-form__input-price');
+const MIN_VALUE_PRICE = 0;
+const MAX_VALUE_PRICE = 1000000;
+
+inputPriceAd.addEventListener('input', () => {
+  const valuePrice = inputPriceAd.value;
+
+  if (valuePrice < MIN_VALUE_PRICE) {
+    inputPriceAd.setCustomValidity('Минимальная цена не должна быть отрицательной');
+  } else if (valuePrice > MAX_VALUE_PRICE) {
+    inputPriceAd.setCustomValidity('Максимальная цена не должна превышать 1000000');
+  } else {
+    inputPriceAd.setCustomValidity('');
+  }
+
+  inputPriceAd.reportValidity();
+});
+
+
+const choiceRooms = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
+
+choiceRooms.addEventListener('click', () => {
+  if () {
+    capacity.remove();
+  }
+});
