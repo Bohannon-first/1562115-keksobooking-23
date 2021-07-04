5449d1da08167112546ea7dddd76470b36744424
Ярослав <spartacus215@yandex.ru>
2021-07-04 22:32:59 +0300

    Д/з Помощь друга(в процессе)

---
+++ js/map.js
@@ -0,0 +1,14 @@
+import {mapCanvas} from './popup.js';
+
+const map = L.map(mapCanvas);
+map.setView({
+  lat: 59.92749,
+  lng: 30.31127,
+}, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
